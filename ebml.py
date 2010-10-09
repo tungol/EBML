fd7c6293254250cc61ee6ab93b4eb0cdc1a4baa9
Stephen <tungolcraeft@gmail.com>
2010-10-08 20:06:51 -0400

    some starts at a writing ability

--- ebml.py
+++ ebml.py
@@ -8,12 +8,15 @@ class EOFError(Exception):
 	pass
 
 class Reference(object):
-	def __init__(self, filename, offset):
+	def __init__(self, doctype, filename=None, offset=None):
+		self.doctype = doctype
 		self.filename = filename
 		self.hexid_offset = offset
+		if filename == offset == None:
+			self.dummy = True
 	
 	def __repr__(self):
-		return "Reference(%r, %r)" % (self.filename, 
+		return "Reference(%r, %r, %r)" % (self.doctype, self.filename, 
 			self.hexid_offset)
 	
 	def __str__(self):
@@ -37,10 +40,17 @@ class Reference(object):
 		elif name == 'total_length':
 			self.total_length = self.hexid_length + self.size_length + self.payload_length
 			return self.total_length
+		elif name == 'valtype':
+			self.valtype = self.doctype.lookup(self.hexid).valtype
+			return self.valtype
+		elif name == 'payload':
+			return self.get_payload()
+		elif name == 'hexid':
+			return self.get_hexid()
 		else:
 			raise AttributeError
 	
-	def get_hexid(self, ids):
+	def get_hexid(self):
 		hexid = ''
 		with open(self.filename, 'rb') as file:
 			file.seek(self.hexid_offset)
@@ -51,10 +61,11 @@ class Reference(object):
 				hexid += byte
 				if len(hexid) > 8:
 					raise EOFError('Should have found an id by now: %r') % hexid
-				if hexid in ids:
+				if hexid in self.doctype.get_ids():
 					break
 		self.hexid_length = len(hexid)
-		return hexid
+		self.hexid = hexid
+		return self.hexid
 	
 	def get_size_length(self):
 		if 'size_length' in dir(self):
@@ -83,34 +94,36 @@ class Reference(object):
 		self.payload_length = bits.uint
 		return self.payload_length
 	
-	def get_payload(self, valtype):
+	def get_payload(self):
+		if valtype == 'container':
+			self.payload = []
+			offset = self.payload_offset
+			while offset != self.end:
+				if offset > self.end:
+					raise EOFError('Went too far, file is damaged.')
+				reference = Reference(self.doctype, self.filename, offset)
+				element = Element(self.doctype, reference)
+				self.payload.append(element)
+				offset += reference.total_length
+			return self.payload
 		raw_payload = bitstring.Bits(filename=self.filename, 
 			offset=self.payload_offset*8, length=self.payload_length*8)
-		if valtype == 'uint':
-			payload = raw_payload.uint
-		elif valtype == 'int':
-			payload = raw_payload.int
-		elif valtype == 'float':
-			payload = raw_payload.float
-		elif valtype == 'string':
-			payload = raw_payload.bytes
-		elif valtype == 'date':
-			payload = raw_payload.int
-		elif valtype == 'binary':
-			payload = raw_payload.bytes
-		return payload
+		if self.valtype == 'uint':
+			self.payload = raw_payload.uint
+		elif self.valtype == 'int':
+			self.payload = raw_payload.int
+		elif self.valtype == 'float':
+			self.payload = raw_payload.float
+		elif self.valtype == 'string':
+			self.payload = raw_payload.bytes
+		elif self.valtype == 'date':
+			self.payload = raw_payload.int
+		elif self.valtype == 'binary':
+			self.payload = raw_payload.bytes
+		return self.payload
 	
-	def get_container_payload(self, doctype):
-		payload = []
-		offset = self.payload_offset
-		while offset != self.end:
-			if offset > self.end:
-				raise EOFError('Went too far, file is damaged.')
-			reference = Reference(self.filename, offset)
-			element = Element(doctype, reference)
-			payload.append(element)
-			offset += reference.total_length
-		return payload
+	def get_delta_size(self, new_payload):
+		new_payload_length = self.
 	
 
 class Element(object):
@@ -118,7 +131,7 @@ class Element(object):
 		if len(args) == 2:
 			self.doctype = args[0]
 			self.reference = args[1]
-			self.hexid = self.reference.get_hexid(self.doctype.get_ids())
+			self.hexid = self.reference.get_hexid()
 			if self.valtype != 'container':
 				if self.reference.payload_length < 16:
 					self.set_payload()
@@ -126,11 +139,13 @@ class Element(object):
 			self.doctype = args[0]
 			self.hexid = hexid[1]
 			self.payload = payload[2]
+		elif len(args) < 2:
+			raise TypeError('__init__() takes at least 2 arguments (%s given)' % len(args))
 		else:
-			raise SyntaxError
+			raise TypeError('__init__() takes at most 3 arguments (%s given)' % len(args))
 	
 	def __repr__(self):
-		if 'reference' in dir(self):
+		if self.has_reference():
 			return 'Element(%r, %r)' % (self.doctype, self.reference)
 		else:
 			return 'Element(%r, %r, %r)' % (self.doctype, self.hexid, self.payload)
@@ -146,7 +161,7 @@ class Element(object):
 			self.name = self.doctype.lookup(self.hexid).name
 			return self.name
 		elif key == 'payload':
-			self.set_payload()
+			self.payload = self.reference.get_payload()
 			return self.payload
 		elif key == 'valtype':
 			self.valtype = self.doctype.lookup(self.hexid).valtype
@@ -158,12 +173,46 @@ class Element(object):
 		if self.valtype == 'container':
 			return iter(self.payload)
 	
-	def set_payload(self):
-		if self.valtype == 'container':
-			self.payload = self.reference.get_container_payload(self.doctype)
-		else:
-			self.payload = self.reference.get_payload(self.valtype)
+	def writes_pending(self):
+		if self.has_reference():
+			if self.payload == self.reference.payload:
+				return False
+			return True
+		return True
 	
+	def has_reference(self):
+		if 'reference' in dir(self):
+			return True
+		return False
+	
+	def get_delta_size(self):
+		if self.writes_pending():
+			if self.has_reference():
+				self.delta_size = self.reference.get_delta_size(self.payload)
+			else:
+				dummy_reference = Reference(self.doctype)
+				self.delta_size = dummy_reference.get_delta_size(self.payload)
+				self.delta_size += len(self.hexid)
+			return self.delta_size
+		return 0
+	
+	def write(starting_shift=0, max_shift=1024, commit=False, upcoming=None):
+		if self.valtype == 'container':
+			results = []
+			for index, child in enumerate(self.payload):
+				if index + 1 == len(self.payload):
+					next = None
+				else:
+					next = self.payload(index + 1)
+				if child.valtype == 'container':
+					result = child.write(shift, max_shift, commit, next)
+				else:
+					result = child.reference.write(shift, max_shift, commit, next)
+				results.append(result)
+				shift += result.shift_delta
+			
+		else:
+			pass
 
 
 class EBML(object):
@@ -184,7 +233,7 @@ class EBML(object):
 	
 	def find_document_type(self):
 		file = open(self.filename, 'rb')
-		reference = Reference(self.filename, 0)
+		reference = Reference(self.doctype, self.filename, 0)
 		first_element = Element(self.doctype, reference)
 		doctype_element_name = 'DocType'
 		found = False
@@ -206,10 +255,14 @@ class EBML(object):
 				break
 			elif offset > end:
 				raise EOFError('Went too far, file is damaged.')
-			reference = Reference(self.filename, offset)
+			reference = Reference(self.doctype, self.filename, offset)
 			element = Element(self.doctype, reference)
 			self.children.append(element)
 			offset = reference.end
+	
+	def write(self):
+		for item in self.children:
+			
 	
 
 
